3d616206c37453d71b23ae0eb45be4ef26d033de
hieutrycoding <dotrunghieu882005@gmail.com>
2026-05-31 01:22:41 +0700

    xog dang chuong va fix db

--- backend/src/routes/manga.routes.js
+++ backend/src/routes/manga.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mangaController = require('../controllers/MangaController');
-
+const chapterController = require('../controllers/ChapterController');
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
@@ -16,7 +16,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'mangaweb_covers', // Ảnh sẽ vào thư mục này
-    allowedFormats: ['jpg', 'png', 'jpeg'],
+    allowedFormats: ['jpg', 'png', 'jpeg', 'webp'],
   },
 });
 
@@ -24,5 +24,6 @@ const upload = multer({ storage: storage });
 
 // Route : POST /api/manga
 router.post('/', upload.single('coverImage'), mangaController.createManga);
-
+router.get('/my-comics', mangaController.getMyMangas);
+router.post('/chapters', upload.array('pages', 50), chapterController.createChapter);
 module.exports = router;
